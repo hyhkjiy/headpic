801ab644c755c1fa8369df356715204681aba096
hyhk_jiy <2632790902@qq.com>
2017-01-05 14:22:21 +0800

    update algorithm.

--- head_pic.py
+++ head_pic.py
@@ -1,35 +1,22 @@
 # coding:utf-8
 import hashlib
 # 导入图像相关模块
-from PIL import Image,ImageDraw
+from PIL import Image, ImageDraw
 
 # 根据字符串生成5*5的0，1矩阵
-def to_hash_array(str):
-    hash_str = hashlib.md5(str).hexdigest()
-    binary_str = bin(int(hash_str,16))[2:][:64]
+def to_hash_array(str_):
+    row=5   # 行数
+    col=5   # 列数
+    hash_str = hashlib.md5(str_).hexdigest()
+    binary_str = bin(int(hash_str,16))[2:][:60:4]
     
-    # print binary_str
+    # 创建二维列表
+    arr = [[0] * col for i in range(row)]
 
-    arrs = [[0] * 5,[0] * 3]
-    for i in range(5):
-        arrs[0][i] = int(binary_str[i*3])
-
-    for i in range(2):
-        arrs[1][i] = int(binary_str[i*3])
-
-    arrs2 = [([0] * 3) for i in range(5)]
-
-    for i in range(5):
-        for j in range(3):
-            arrs2[i][j] = arrs[0][i]|arrs[1][j]
-
-    arrs3 = [([0] * 5) for i in range(5)]
-    for i in range(5):
-        for j in range(3):
-            arrs3[i][4-j] = arrs2[i][j]
-            arrs3[i][j] = arrs2[i][j]
-    return arrs3
-
+    for x, y, i in [(i%row, i%((col+1)/2), binary_str[i]) for i in range(len(binary_str))]:
+        arr[x][y] = int(i)
+        arr[x][row - 1 - y] = int(i)
+    return arr
 
 # 根据字符串的hash生成颜色值
 def get_hash_color(str):
@@ -53,28 +40,12 @@ def get_hash_image(str='default',width=250,height=250,rect_width=50,rect_height=
     arr = to_hash_array(str)
     for i in range(len(arr)):
         for j in range(len(arr[i])):
-            #fill = (arr[i][j] == 1) ? rectcolor : None
-            fill = None
-            if arr[i][j] == 1:
-                fill = rectcolor
+            fill = rectcolor if arr[i][j] == 1 else None
             draw.rectangle((j*rect_width,i*rect_height,(j+1)*rect_width,(i+1)*rect_height),fill=fill)
     # 释放draw
     del draw
     return image
 
 
-img = get_hash_image('123123')
+img = get_hash_image()
 img.save('1234_1.jpeg')
-
-# from datetime import datetime
-# import time
-# def get_file_name():
-#    timestamp = str(int(time.mktime(datetime.utcnow().timetuple())))
-#    file_name = 'head_pic_'+timestamp+'.jpge'
-#    return file_name
-# print get_file_name()
-
-# import os
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# head_dir = basedir + '\\project\\static\\images\\heads\\'
-# print head_dir
